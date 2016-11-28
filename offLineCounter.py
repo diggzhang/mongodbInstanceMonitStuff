46886c8fa732cd194bbc6e207b6f99c0d1efc62d
diggzhang <diggzhang@gmail.com>
2016-11-28 12:18:30 +0800

    add post mail function

--- offLineCounter.py
+++ offLineCounter.py
@@ -12,6 +12,7 @@ local_count - count 看值
 """
 
 import pymongo
+import sys
 from pymongo import MongoClient
 from datetime import datetime
 import datetime
@@ -22,11 +23,52 @@ from datetime import date
 import calendar
 import subprocess
 import dateutil.parser
+import smtplib
+from email.mime.text import MIMEText
 
 
 db_host = MongoClient("10.8.8.111", 27017)
 db_events_v4 = db_host["eventsV4"]
 collections_eventV4 = db_events_v4['eventV4']
+
+
+# mail configure
+mailto_list = ["1192309514@qq.com"]
+mail_host = "smtp.126.com"
+mail_user = "guangheauto"
+mail_pass = "hello123"  # guanghedev
+mail_postfix = "126.com"
+
+
+def sendMail(toList, subject, content):
+    me = mail_user + "<" + mail_user + "@" + mail_postfix + ">"
+    msg = MIMEText(content)
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = ";".join(toList)
+    try:
+        s = smtplib.SMTP()
+        s.connect(mail_host)
+        s.login(mail_user, mail_pass)
+        s.sendmail(me, toList, msg.as_string())
+        s.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+
+
+def post_man(mail_title, mail_log):
+    mail_title = mail_title + " Count WARNING"
+    try:
+        if sendMail(mailto_list, mail_title, mail_log):
+            print("[*] SEND SUCCESS title: %s") % (mail_title)
+            sys.exit()
+        else:
+            print("[*] SEND FAILURE title: %s") % (mail_title)
+    except Exception, e:
+        print(str(e))
+        raise
 
 
 class get_collection_start_end_count(object):
@@ -82,8 +124,10 @@ BASE_DIFF_COUNT = 1000
 diff_count = online_docs_count - local_docs_count
 if abs(diff_count) >= BASE_DIFF_COUNT:
     print("线上线下数据量差距大于1000")
-    print("online %s, offline %s, diff: %s")%(online_docs_count, local_docs_count, diff_count)
+    warnning_info = ("online %s, offline %s, diff: %s")%(online_docs_count, local_docs_count, diff_count)
     print("Send WARNING MAIL")
+    post_man("线上线下数据量差距大于1000", warnning_info)
 else:
     print("线上 - 线下 数据量")
     print("online %s, offline %s, diff: %s")%(online_docs_count, local_docs_count, diff_count)
+    # post_man("线上线下数据量差距大于1000", str(diff_count))
